7aca050acc0742c0e4d91dd1c2bf25b0d9c91df6
Anton Isakov <tonias@yandex.ru>
2024-12-17 18:24:13 +0300

    Create CommonStructsAndAlgos project.

---
+++ CommonStructsAndAlgos/CharMap.cs
@@ -0,0 +1,23 @@
+﻿namespace CommonStructsAndAlgos;
+
+public class CharsMap : Map<char>
+{
+    public CharsMap(string[] lines)
+    {
+        var width = lines[0].Length;
+        var height = lines.Length;
+        char[,] map = new char[width, height];
+
+        for (int y = 0; y < lines.Length; y++)
+        {
+            var line = lines[y];
+            if (line.Length == 0)
+                break;
+            for (int x = 0; x < line.Length; x++)
+            {
+                map[x, y] = line[x];
+            }
+        }
+        MapData = map;
+    }
+}
---
+++ CommonStructsAndAlgos/Map.cs
@@ -0,0 +1,15 @@
+﻿namespace CommonStructsAndAlgos;
+
+public class Map<T>
+{
+    protected T[,] MapData = new T[0, 0];
+
+    public T this[int x, int y]
+    {
+        get => MapData[x, y];
+        set => MapData[x, y] = value;
+    }
+
+    public int Width => MapData.GetLength(0);
+    public int Height => MapData.GetLength(1);
+}
