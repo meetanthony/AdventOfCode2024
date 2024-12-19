2339a2a5b61be7f8e2e616d668734acabeb2f2ce
Anton Isakov <tonias@yandex.ru>
2024-12-19 22:52:34 +0300

    AdventOfCode.Day18.Problem01 and AdventOfCode.Day18.Problem02 solved

---
+++ AdventOfCode.Day18.Problem0102/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using AdventOfCode.Day18.Problem0102.SubTypes;
+using CommonStructsAndAlgos;
+
+namespace AdventOfCode.Day18.Problem0102;
+
+internal class Program
+{
+    private static void Main()
+    {
+        var example = false;
+
+        Map<bool> map;
+        string fileName;
+        int fallenBytesCount;
+
+
+        if (example)
+        {
+            map = new Map<bool>(7, 7);
+            fileName = "TestData\\example.txt";
+            fallenBytesCount = 12;
+        }
+        else
+        {
+            map = new Map<bool>(71, 71);
+            fileName = "TestData\\day18.txt";
+            fallenBytesCount = 1024;
+        }
+
+        var lines = File.ReadAllLines(fileName);
+
+        var fallingBytes = new List<Point>(lines.Length);
+        foreach (var line in lines)
+        {
+            var coordComponents = line.Split(',');
+            var x = int.Parse(coordComponents[0]);
+            var y = int.Parse(coordComponents[1]);
+            fallingBytes.Add(new Point(x, y));
+        }
+        
+        var sw = Stopwatch.StartNew();
+
+        for (var bytesFallen = 0; bytesFallen < fallenBytesCount; bytesFallen++)
+        {
+            var fallingByte = fallingBytes[bytesFallen];
+            map[fallingByte] = true;
+        }
+
+        // Problem 01
+        {
+            var shortestPaths = PathFinder.GetShortestPathes(map, new[] { true }, Point.Empty,
+                new Point(map.Width - 1, map.Height - 1));
+
+            Console.WriteLine(shortestPaths[0].Count - 1);
+        }
+
+        // Problem 02
+        var oldShortestPath = new List<Point>();
+        for (int bytesFallen = fallenBytesCount; bytesFallen < fallingBytes.Count; bytesFallen++)
+        {
+            var fallingByte = fallingBytes[bytesFallen];
+            map[fallingByte] = true;
+            if (oldShortestPath.Count > 0 && oldShortestPath.Contains(fallingByte) == false)
+                continue;
+
+            var shortestPaths = PathFinder.GetShortestPathes(map, new[] { true }, Point.Empty,
+                new Point(map.Width - 1, map.Height - 1));
+            
+            if (shortestPaths.Count == 0)
+            {
+                Console.WriteLine(fallingByte);
+                break;
+            }
+            oldShortestPath = shortestPaths[0];
+        }
+
+        Console.WriteLine(sw.Elapsed.ToString());
+    }
+
+    private static void PrintMap(Map<bool> map)
+    {
+        var startPoint = Console.GetCursorPosition();
+        foreach (var mapElement in map)
+        {
+            Console.SetCursorPosition(mapElement.Coords.X, mapElement.Coords.Y);
+            Console.Write(mapElement.Value ? '#' : '.');
+        }
+
+        Console.SetCursorPosition(startPoint.Left, startPoint.Top);
+    }
+
+    private static void PrintPath(List<Point> path, Map<bool> map)
+    {
+        for (var y = 0; y < map.Height; y++)
+        {
+            for (var x = 0; x < map.Width; x++)
+            {
+                Console.SetCursorPosition(x, y);
+                Console.Write(path.Contains(new Point(x, y)) ? "O" : (map[new Point(x, y)] ? "#" : "."));
+            }
+        }
+    }
+}
---
+++ AdventOfCode.Day18.Problem0102/SubTypes/PathFinder.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using CommonStructsAndAlgos;
+
+namespace AdventOfCode.Day18.Problem0102.SubTypes;
+
+public static class PathFinder
+{
+    public static bool Print { get; set; } = false;
+
+    public static List<List<Point>> GetShortestPathes<T>(Map<T> map, T[] wallsTypes, Point currentCoords, Point endCoords)
+    {
+        var shortestPaths = new List<List<Point>>();
+        var currentPath = new List<Point>();
+        var coordScores = new Map<int>(map.Width, map.Height);
+        foreach (var coordScore in coordScores)
+        {
+            coordScores[coordScore.Coords] = int.MaxValue;
+        }
+        var visitMap = new Map<bool>(map.Width, map.Height);
+
+        foreach (var mapElement in map)
+        {
+            var x = mapElement.Coords.X;
+            var y = mapElement.Coords.Y;
+            if (Print) Console.SetCursorPosition(x, y);
+
+            if (wallsTypes.Contains(mapElement.Value))
+            {
+                visitMap[mapElement.Coords] = true;
+                if (Print) Console.Write("#");
+            }
+            else if (Print) Console.Write(".");
+        }
+
+        GetShortestPathes(currentCoords, endCoords, currentPath, coordScores, visitMap, shortestPaths);
+
+        return shortestPaths;
+    }
+
+    private static void GetShortestPathes(Point currentCoords, Point endCoords,
+            List<Point> currentPath, Map<int> coordScores, Map<bool> visitMap, List<List<Point>> shortestPaths)
+    {
+        var x = currentCoords.X;
+        var y = currentCoords.Y;
+        if (Print)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write("0");
+        }
+
+        currentPath.Add(currentCoords);
+        var currentPathLength = currentPath.Count + (endCoords.X - currentCoords.X) + (endCoords.Y - currentCoords.Y);
+
+        if (coordScores[currentCoords] <= currentPathLength)
+            goto Exit;
+
+        coordScores[currentCoords] = currentPathLength;
+
+        var shortestPathLength = shortestPaths.Count == 0 ? int.MaxValue : shortestPaths[0].Count;
+
+        if (currentCoords == endCoords)
+        {
+            if (currentPathLength < shortestPathLength)
+            {
+                shortestPaths.Clear();
+            }
+
+            List<Point> shortestPath = new List<Point>(currentPath);
+
+            shortestPaths.Add(shortestPath);
+
+            goto Exit;
+        }
+
+        if (currentPathLength > shortestPathLength)
+            goto Exit;
+
+        visitMap[currentCoords] = true;
+        var nextLocations = GetPossibleNextLocations(visitMap, currentCoords);
+        foreach (var nextLocation in nextLocations)
+        {
+            GetShortestPathes(nextLocation, endCoords, currentPath, coordScores, visitMap, shortestPaths);
+        }
+
+        visitMap[currentCoords] = false;
+
+    Exit:;
+
+        if (Print)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(".");
+        }
+
+        currentPath.RemoveAt(currentPath.Count - 1);
+    }
+
+    private static Point[] GetPossibleNextLocations(Map<bool> visitMap, Point currentCoords)
+    {
+        var possibleNextLocations = new List<Point>();
+
+        var possibleNextLocation = new Point(currentCoords.X + 1, currentCoords.Y);
+
+        if (possibleNextLocation.X < visitMap.Width && visitMap[possibleNextLocation] == false)
+            possibleNextLocations.Add(possibleNextLocation);
+
+        possibleNextLocation = new Point(currentCoords.X, currentCoords.Y + 1);
+        if (possibleNextLocation.Y < visitMap.Height && visitMap[possibleNextLocation] == false)
+            possibleNextLocations.Add(possibleNextLocation);
+
+        possibleNextLocation = new Point(currentCoords.X - 1, currentCoords.Y);
+        if (possibleNextLocation.X >= 0 && visitMap[possibleNextLocation] == false)
+            possibleNextLocations.Add(possibleNextLocation);
+
+        possibleNextLocation = new Point(currentCoords.X, currentCoords.Y - 1);
+        if (possibleNextLocation.Y >= 0 && visitMap[possibleNextLocation] == false)
+            possibleNextLocations.Add(possibleNextLocation);
+
+        return possibleNextLocations.ToArray();
+    }
+}
